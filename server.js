ed225a14c118e1f383a6e2a9b3205b32988c18b1
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-03 10:38:25 -0400

    reinstall express and mongoose

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
@@ -20,15 +20,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+//Connect to mongodb atlas and server
+// mongoose.connect("mongodb://localhost/budget", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
