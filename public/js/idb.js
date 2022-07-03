e8fffaf28191855aeb6bfdf31c016030d8a1c796
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-02 21:37:48 -0400

    added requests and function to db

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,6 +7,38 @@ const request = indexedDB.open('PWA-19', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_', { autoIncrement: true });
+    // create an object store (table) called `new_transaction`, 
+    //set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
   };
+
+  // upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store established a connection, 
+    //save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      // uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
+
+  // This function will be executed if we attempt to submit a new transaction
+  // and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_transaction`
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    transactionObjectStore.add(record);
+  }
