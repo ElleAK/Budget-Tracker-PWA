616cb00a1bebe94f36e0c049187d0b3d11944e3d
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-03 12:57:38 -0400

    server 127.0.0.1 and db name update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:127.0.0.1/budget-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
