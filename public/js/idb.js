67003c5e5f82fc2bc23183554d9adaeec6c98693
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-02 20:33:21 -0400

    idb onupgradeneeded

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,6 +1,6 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+// establish a connection to IndexedDB database called 'PWA-19' and set it to version 1
 const request = indexedDB.open('PWA-19', 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
@@ -8,5 +8,5 @@ request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
     // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_pizza', { autoIncrement: true });
+    db.createObjectStore('new_', { autoIncrement: true });
   };
