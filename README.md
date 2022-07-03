c18c11cb02b5e4734ab225a25c210ca189c9b108
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-02 23:32:33 -0400

    added manifest json and completed readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Budget Tracker Starter Code
+# Budget Tracker
+
+  ## Table of Contents:
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Technologies](#technologies)
+  - [Contributing](#contributing)
+  - [Author](#author)
+  - [Website](#link)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Description:
+  #### This application allows you to update your budget even when you are offline. Any changes made while offline will be updated once there is an internet connection.
+
+  ## Installation:
+  #### Before using this application please install Express and Mongoose.
+
+  ## Usage:
+  #### To use this application first clone the repository and navigate to the root folder in the command-line. Install Express and Mongoose, type npm start to start run the Budget Tracker. 
+
+  ## Technologies:
+  #### Javascript, Node
+
+  ## Contributing:
+  #### Will not be accepting contributions at this time.
+  
+  ## Author:
+  #### Lindsey Kelly
+
+  ## Deployed Site:
+  #### https://github.com/ElleAK/README-Generator
+
+  ## License: ![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)
+  #### https://opendatacommons.org/licenses/pddl/
+
+  ## Questions:
+  ### If you have any questions regarding this repository please contact me by GitHub:
+  #### username: ElleAK
+  ### Or email: nightbirdphotos@gmail.com
--- public/service-worker.js
+++ public/service-worker.js
@@ -3,30 +3,14 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    ".js/idb.js",
-    ".js/index.js",
-    ".assets/css/style.css",
+    "/index.html",
+    "/js/idb.js",
+    "/js/index.js",
+    "/css/styles.css"
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-    })
-    )
-  })
-
   // Cache resources
-self.addEventListener('install', function (e) {
+  self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
         console.log('installing cache : ' + CACHE_NAME)
@@ -35,7 +19,7 @@ self.addEventListener('install', function (e) {
     )
   })
 
-  // Delete outdated caches
+    // Delete outdated caches
 self.addEventListener('activate', function (e) {
     e.waitUntil(
       caches.keys().then(function (keyList) {
@@ -56,3 +40,20 @@ self.addEventListener('activate', function (e) {
       })
     );
   });
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+    })
+    )
+  })
+
